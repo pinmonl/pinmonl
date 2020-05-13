735941754c30e19b93775c92478a3e5565b098c6
Ah Shum <ahshum911@gmail.com>
2020-05-13 16:58:06 +0800

    fix(webui): follow eslint rule

--- webui/src/store/ws.js
+++ webui/src/store/ws.js
@@ -4,8 +4,7 @@ export default {
       const ws = new WebSocket(`ws://${location.host}/ws`)
       ws.onopen = () => {
       }
-      ws.onmessage = (event) => {
-        console.log(JSON.parse(event.data))
+      ws.onmessage = () => {
       }
     },
   },
